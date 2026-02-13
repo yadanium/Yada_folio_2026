9e88234e2fe84f8b2490443064b09446b48f625f
Yadanium <yadamisuzu3122@gmail.com>
2026-02-13 16:33:04 +0900

    add_links

--- src/app/data/media.ts
+++ src/app/data/media.ts
@@ -6,9 +6,52 @@ export interface MediaItem {
 
 export const mediaItems: MediaItem[] = [
   {
-    date: "2026-02-13",
-    title: "ツイッター",
-    url: "https://x.com/yada_kaeru",
+    date: "2025.8.17",
+    title: "siro Inc.  Shinya Matsuyama note 「量子センサーとインタラクティブ」",
+    url: "https://note.com/gff02521/n/n16f3aae4f1ab",
+  },
+  {
+    date: "2025.7.10",
+    title:
+      "「パテントコンテスト／デザインパテントコンテスト」受賞者に聞いた 特許＆意匠権取得までのストーリー",
+    url: "https://prtimes.jp/story/detail/B4eyW8c4X1x",
+  },
+  {
+    date: "2024.6.7",
+    title: "連載インタビューtueks x...　第五回 tueks x ヤダニウム",
+    url: "https://www.tueks3.com/tueks-x-yadanium",
+  },
+  {
+    date: "2024.4.17",
+    title:
+      "デザインパテントコンテスト受賞者インタビュー「完成までの道のりから受賞へ」",
+    url: "https://www.youtube.com/watch?v=KQrpc8UFBJY",
+  },
+  {
+    date: "2024.3.25",
+    title:
+      "デザインノトビラ　2023年度 千葉工業大学 創造工学部 デザイン科学科 卒業研究展",
+    url: "https://skhole.japandesign.ne.jp/sotsuten/chiba-tech-2024/",
+  },
+  {
+    date: "2024.2.15",
+    title: "SONY Technology Exchange Fairレポート",
+    url: "https://sony-acceleration-platform.com/article1165.html",
+  },
+  {
+    date: "2023.12.27",
+    title: "看板おしゃれデザインコンペ表彰式を開催",
+    url: "https://kyodokogei.co.jp/news/news-4521/",
+  },
+  {
+    date: "2023.12.23",
+    title: "毎日新聞　デザインコンペ表彰式",
+    url: "https://mainichi.jp/articles/20231223/ddl/k12/040/137000c",
+  },
+  {
+    date: "2023.11.27",
+    title: "ロゴマークに込めた想い～Bonheur～",
+    url: "https://camp-fire.jp/projects/670333/view/activities/528838#main",
   },
   // 追加のメディア掲載はこの配列に追記してください
 ];
